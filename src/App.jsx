7d27820f314e35e3cc0afc56871cac1dd3887077
ramez <ramezkhaled259@gmail.com>
2025-11-02 15:10:27 +0200

    Adding pages files & its css modules files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-function App() {
-  const x = 5;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
-  return <div>WorldWise</div>;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
